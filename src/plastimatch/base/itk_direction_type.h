753f6adb57b0099abfca63efa8e4b950f7b80b36
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2022-10-31 16:49:55 -0400

    Add missing file from CUDA fix

---
+++ src/plastimatch/base/itk_direction_type.h
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#ifndef _itk_direction_type_h_
+#define _itk_direction_type_h_
+
+#include "itkMatrix.h"
+
+typedef itk::Matrix < double, 3, 3 > DirectionType;
+
+#endif
