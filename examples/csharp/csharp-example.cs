238e8ae08c3e10e05cfaed9696262f846ab03060
Gregory Sharp <gcsharp@partners.org>
2022-12-02 10:22:15 -0500

    Fix csharp-example code

---
+++ examples/csharp/csharp-example.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var k = new Gamma_dose_comparison();
+        }
+    }
+}
--- src/csharp/csharp-example.cs
+++
@@ -1,6 +0,0 @@
-﻿static void Main(string[] args)
-{
-
-    Console.WriteLine("Hello, World!");
-    var k = new Gamma_dose_comparison();
-}
